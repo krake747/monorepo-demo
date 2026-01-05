5bb92ce1d2fa2846e60b525e4a1643f7cb17b9aa
Kevin Kraemer <kraemer.kevin747@gmail.com>
2026-01-05 21:33:26 +0100

    test button

--- apps/linear-dream/src/App.tsx
+++ apps/linear-dream/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
             <div className="card">
                 <Button onClick={() => setCount(incrementOne(count))}>Click Here from @ui</Button>
                 <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
+                    Edit <code>src/App.tsx</code> and save to test HMR {count}
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
