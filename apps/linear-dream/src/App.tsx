7afff509b94065a57f8436e9b3b9895f18155471
Kevin Kraemer <kraemer.kevin747@gmail.com>
2026-01-05 21:31:30 +0100

    reference package ui in linear dream

--- apps/linear-dream/src/App.tsx
+++ apps/linear-dream/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { adder } from "@mono/core";
+import { Button } from "@mono/ui";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -21,7 +22,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => incrementOne(count))}>count is {count}</button>
+                <Button onClick={() => setCount(incrementOne(count))}>Click Here from @ui</Button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
