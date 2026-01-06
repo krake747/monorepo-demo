14061821941274c6ad3f742b61097cac6caf1600
Kevin Kraemer <kraemer.kevin747@gmail.com>
2026-01-06 21:05:26 +0100

    setup ui

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
 
-export function Button({ children, onClick }: React.ComponentProps<"button">) {
-    return <button onClick={onClick}>{children}</button>;
+export function Button({ children, onClick, ...props }: React.ComponentProps<"button">) {
+    return (
+        <button
+            onClick={onClick}
+            className="ui:flex ui:items-center ui:justify-center ui:px-4 ui:py-2 ui:bg-blue-500 ui:text-white ui:rounded-md ui:hover:bg-blue-600 ui:transition-colors"
+            {...props}
+        >
+            {children}
+        </button>
+    );
 }
